f17ae4e472144bdc9ef496204d663fefc58404e2
athakkar2 <athakka5@stevens.edu>
2022-12-10 20:07:09 -0500

    atmlight function progress

--- dehazer.cpp
+++ dehazer.cpp
@@ -1,8 +1,24 @@
+#include <math.h>
+
+#include <algorithm>
 #include <iostream>
+#include <numeric>
 #include <opencv2/opencv.hpp>
+#include <vector>
 
 using namespace cv;
 using namespace std;
+
+vector<int> argsort(const vector<float> &array) {
+  vector<int> indices(array.size());
+  iota(indices.begin(), indices.end(), 0);
+  sort(indices.begin(), indices.end(), [&array](int left, int right) -> bool {
+    // sort indices according to corresponding array element
+    return array[left] < array[right];
+  });
+
+  return indices;
+}
 
 void darkChannel(Mat &img, Mat &darkdst) {
   Mat bands[3];
@@ -13,13 +29,30 @@ void darkChannel(Mat &img, Mat &darkdst) {
   erode(dc, darkdst, kernel);
 }
 
-void AtmLight(Mat &img, Mat &Adst, Mat &dark) {}
+void AtmLight(Mat &img, Mat &Adst, Mat &dark) {
+  int imgsz = img.rows * img.cols;
+  int numpx = int(max(floor(imgsz / 1000), 1.0));
+  Mat darkvec, imvec;
+  darkvec = dark.reshape(0, imgsz);
+  imvec = img.reshape(3, imgsz);
+  darkvec.convertTo(darkvec, CV_32FC1);
+  vector<float> darkvec1(darkvec.rows * darkvec.cols * darkvec.channels());
+  if (darkvec.isContinuous()) {
+    darkvec1.assign(darkvec.data,
+                    darkvec.data + darkvec.total() * darkvec.channels());
+  }
+  vector<int> indices;
+  indices = argsort(darkvec1);
+  auto iterator = indices.begin();
+  indices.erase(iterator, iterator + imgsz - numpx);
+}
 
 int main(int argc, char **argv) {
   Mat img = imread("low.png");
   bitwise_not(img, img);
   Mat darkdst, A;
-  darkChannel(img, darkdst, 15);
+  darkChannel(img, darkdst);
+  AtmLight(img, A, darkdst);
   namedWindow("Display Image OG", WINDOW_AUTOSIZE);
   imshow("Display Image OG", img);
   namedWindow("Display Image", WINDOW_AUTOSIZE);
