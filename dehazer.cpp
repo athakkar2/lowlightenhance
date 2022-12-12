925651ff2ea32e1b482118f203e4e79f44935824
athakkar2 <athakka5@stevens.edu>
2022-12-11 19:28:25 -0500

    transestimate

--- dehazer.cpp
+++ dehazer.cpp
@@ -25,7 +25,8 @@ void darkChannel(Mat &img, Mat &darkdst) {
   split(img, bands);
   Size a(15, 15);
   Mat dc = min(min(bands[0], bands[1]), bands[2]);
-  Mat kernel = getStructuringElement(MORPH_RECT, a);
+  Mat kernel;
+  getStructuringElement(MORPH_RECT, a).convertTo(kernel, CV_32F);
   erode(dc, darkdst, kernel);
 }
 
@@ -33,6 +34,8 @@ void AtmLight(Mat &img, float A[], Mat &dark) {
   int imgsz = img.rows * img.cols;
   int numpx = int(max(floor(imgsz / 1000), 1.0));
   Mat darkvec, imvec;
+  darkvec.convertTo(darkvec, CV_32F);
+  imvec.convertTo(imvec, CV_32F);
   darkvec = dark.reshape(0, imgsz);
   imvec = img.reshape(3, imgsz);
   vector<float> darkvec1(darkvec.rows * darkvec.cols * darkvec.channels());
@@ -57,22 +60,44 @@ void AtmLight(Mat &img, float A[], Mat &dark) {
   }
 }
 
+void transmissionEstimate(Mat &img, float A[], Mat &trans) {
+  float omega = .95;
+  Mat im3;
+  im3.convertTo(im3, CV_32F);
+  Mat bands[3];
+  split(img, bands);
+  for (int i = 0; i < 3; i++) {
+    bands[i].convertTo(bands[i], CV_32F, 1.0 / (A[i]));
+  }
+  merge(bands, 3, im3);
+  Mat dark;
+  dark.convertTo(dark, CV_32F);
+  darkChannel(im3, dark);
+  dark.convertTo(trans, -1, omega);
+  trans = 1 - trans;
+}
+
 int main(int argc, char **argv) {
   Mat img = imread("Low.png");
   Mat imgCopy;
   bitwise_not(img, img);
-  img.convertTo(imgCopy, -1, 1.0 / 255, 0);
+  img.convertTo(imgCopy, CV_32F, (1.0 / 255), 0);
   Mat darkdst;
+  darkdst.convertTo(darkdst, CV_32F);
+  Mat trans;
   // darkdst.convertTo(darkdst, CV_32FC3, 1.0 / 255);
   float A[3] = {};
   darkChannel(img, darkdst);
   AtmLight(img, A, darkdst);
-  // img.convertTo(img, CV_8UC3, 255);
-  // darkdst.convertTo(darkdst, CV_8UC3, 255);
+  transmissionEstimate(imgCopy, A, trans);
+  //  img.convertTo(img, CV_8UC3, 255);
+  // darkdst.convertTo(darkdst, CV_8U, 255);
   namedWindow("Display Image OG", WINDOW_AUTOSIZE);
-  imshow("Display Image OG", img);
-  namedWindow("Display Image", WINDOW_AUTOSIZE);
-  imshow("Display Image", darkdst);
+  imshow("Display Image OG", imgCopy);
+  namedWindow("Display Image Dark", WINDOW_AUTOSIZE);
+  imshow("Display Image Dark", darkdst);
+  namedWindow("Display Image TE", WINDOW_AUTOSIZE);
+  imshow("Display Image TE", trans);
   waitKey(0);
   return 0;
 }
