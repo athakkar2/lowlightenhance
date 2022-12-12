f459a5e22da9eeb8c68fa874a7c1371d829341d7
athakkar2 <athakka5@stevens.edu>
2022-12-11 22:30:05 -0500

    final?

--- dehazer.cpp
+++ dehazer.cpp
@@ -77,27 +77,87 @@ void transmissionEstimate(Mat &img, float A[], Mat &trans) {
   trans = 1 - trans;
 }
 
+void guidedFilter(Mat &img, Mat &trans, int r, float eps, Mat &filter) {
+  Mat mean_I, mean_p, meanIp;
+  meanIp = img.mul(trans);
+  Size a(r, r);
+  boxFilter(img, mean_I, CV_64F, a);
+  boxFilter(trans, mean_p, CV_64F, a);
+  boxFilter(meanIp, meanIp, CV_64F, a);
+  Mat cov_Ip = mean_I.mul(mean_p);
+  cov_Ip = meanIp - cov_Ip;
+  Mat mean_II;
+  boxFilter(img.mul(img), mean_II, CV_64F, a);
+  Mat var_I = mean_I.mul(mean_I);
+  var_I = mean_II - var_I;
+  Mat a2 = var_I + eps;
+  a2 = cov_Ip / a2;
+  Mat b = a2.mul(mean_I);
+  b = mean_p - b;
+  Mat mean_a, mean_b;
+  boxFilter(a2, mean_a, CV_64F, a);
+  boxFilter(b, mean_b, CV_64F, a);
+  Mat imgplace;
+  img.convertTo(imgplace, CV_64F);
+  mean_a = mean_a.mul(imgplace);
+  filter = mean_a + mean_b;
+}
+
+void transmissionRefine(Mat &img, Mat &trans, Mat &filter) {
+  Mat gray;
+  cvtColor(img, gray, COLOR_BGR2GRAY);
+  gray.convertTo(gray, CV_32F, 1.0 / 255);
+  int r = 60;
+  float eps = .0001;
+  guidedFilter(gray, trans, r, eps, filter);
+}
+
+void recover(Mat &img, Mat &trans, float A[], Mat &im3) {
+  trans.convertTo(trans, CV_32F);
+  Mat bands[3];
+  split(img, bands);
+  for (int i = 0; i < 3; i++) {
+    bands[i] = bands[i] - A[i];
+    bands[i] = bands[i] / trans;
+    bands[i] = bands[i] + A[i];
+  }
+  merge(bands, 3, im3);
+}
+
 int main(int argc, char **argv) {
-  Mat img = imread("Low.png");
+  Mat img = imread("pen2.jpg");
   Mat imgCopy;
   bitwise_not(img, img);
   img.convertTo(imgCopy, CV_32F, (1.0 / 255), 0);
   Mat darkdst;
   darkdst.convertTo(darkdst, CV_32F);
   Mat trans;
+  Mat filter;
+  Mat fin;
   // darkdst.convertTo(darkdst, CV_32FC3, 1.0 / 255);
   float A[3] = {};
   darkChannel(img, darkdst);
   AtmLight(img, A, darkdst);
   transmissionEstimate(imgCopy, A, trans);
-  //  img.convertTo(img, CV_8UC3, 255);
-  // darkdst.convertTo(darkdst, CV_8U, 255);
+  transmissionRefine(img, trans, filter);
+  recover(imgCopy, filter, A, fin);
+  threshold(fin, fin, 0, 0, THRESH_TOZERO);
+  threshold(fin, fin, 1, 0, THRESH_TRUNC);
+  fin.convertTo(fin, CV_8U, 255);
+  bitwise_not(fin, fin);
+
+  //   img.convertTo(img, CV_8UC3, 255);
+  //  darkdst.convertTo(darkdst, CV_8U, 255);
   namedWindow("Display Image OG", WINDOW_AUTOSIZE);
   imshow("Display Image OG", imgCopy);
   namedWindow("Display Image Dark", WINDOW_AUTOSIZE);
   imshow("Display Image Dark", darkdst);
   namedWindow("Display Image TE", WINDOW_AUTOSIZE);
   imshow("Display Image TE", trans);
+  namedWindow("Display Image filter", WINDOW_AUTOSIZE);
+  imshow("Display Image filter", filter);
+  namedWindow("New Image", WINDOW_NORMAL);
+  imshow("New Image", fin);
   waitKey(0);
   return 0;
 }
